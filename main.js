7f386be7afac07afb7df0f713404b219a0c91e31
Roman <romas6ka@i.ua>
2018-11-23 15:32:59 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-C:\Users\romas\Desktop\maket
+$(window).on('load', function(){
+	$('#1').click( function(){
+		$('header').css('background', 'url(./img/img1.jpg)');
+	})
+	$('#2').click( function(){
+		$('header').css('background', 'url(./img/img.jpg)');
+	})
+	$('#3').click( function(){
+		$('header').css('background', 'url(./img/img1.jpg)');
+	})
+	$('#4').click( function(){
+		$('header').css('background', 'url(./img/img.jpg)');
+	})
+})
+
+$(function(){
+  $('.circle').click(function(){
+    $('.circle').removeClass("active");
+    $(this).addClass("active");
+});
+});
